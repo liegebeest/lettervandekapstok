f73de8365a16ee40577489bf44e4408ed999ac72
Joris Custers <joris@amplidata.com>
2017-08-03 10:49:02 +0200

    nuking file with creds

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = mysqli_connect("lettervandekapstok.be.mysql","lettervandekapstok_be","lettervandekapstok","lettervandekapstok_be");
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL";
-}
-#var_dump($con);
-?>
