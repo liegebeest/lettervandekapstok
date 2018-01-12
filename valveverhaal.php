e6e70f6c94606b2dcdae73015ae7cca1a2507dfc
Joris Custers <6427796+liegebeest@users.noreply.github.com>
2018-01-12 17:10:39 +0100

    Add files via upload

--- valveverhaal.php
+++ valveverhaal.php
@@ -1,26 +1,35 @@
 <?php 
-	require 'db.php';
- 	$sqltran = mysqli_query($con, "SELECT titel, raw, inleiding, genre FROM verhaaltjes ORDER BY id DESC LIMIT 1") or die(mysqli_error($con));
-	$arrVal = array();
- 	$i=1;
- 	while ($rowList = mysqli_fetch_array($sqltran)) {
- 	    
- 	    $rawdata = base64_decode($rowList['raw']);
- 	    $rawdata_print = str_replace("|", "</p><p>", $rawdata);
- 	    
- 	    $inl_raw = $rowList['inleiding'];
- 	    $inl_print = base64_decode($inl_raw);
- 	    
-		$x = array(
-		    'titel'=> $rowList['titel'],
-			'inleiding'=> $inl_print ,
-			'raw'=> $rawdata_print,
-			'genre'=> $rowList['genre'],
-    		);		
-		array_push($arrVal, $x);	
-		$i++;			
- 	}
- 	echo  json_encode($arrVal);		
- 	mysqli_close($con);
+require 'db.php';
+$id = $_GET['id'];
+if($id == NULL)
+{
+    $sqltran = mysqli_query($con, "SELECT titel, raw, inleiding, genre FROM verhaaltjes ORDER BY id DESC LIMIT 1") or die(mysqli_error($con));
+}
+else
+{
+    $sqltran = mysqli_query($con, "SELECT titel, raw, inleiding, genre FROM verhaaltjes WHERE id='" . $id . "'") or die(mysqli_error($con));
+}
+$arrVal = array();
+$i=1;
+while ($rowList = mysqli_fetch_array($sqltran)) 
+{
+    $rawdata = base64_decode($rowList['raw']);
+    $rawdata_print = str_replace("|", "</p><p>", $rawdata);
+    
+    $inl_raw = $rowList['inleiding'];
+    $inl_print = base64_decode($inl_raw);
+    
+    $x = array(
+    'titel'=> $rowList['titel'],
+    'inleiding'=> $inl_print ,
+    'raw'=> $rawdata_print,
+    'genre'=> $rowList['genre'],
+    );		
+    array_push($arrVal, $x);	
+    $i++;			
+}
+
+echo  json_encode($arrVal);		
+mysqli_close($con);
 ?>   
  
--- verhaaltjes.php
+++ verhaaltjes.php
@@ -1,17 +1,18 @@
 <?php 
 	require 'db.php';
- 	$sqltran = mysqli_query($con, "SELECT link, bron, datum, titel, inleiding, raw, genre FROM verhaaltjes") or die(mysqli_error($con));
+ 	$sqltran = mysqli_query($con, "SELECT id, link, bron, datum, titel, inleiding, raw, genre FROM verhaaltjes") or die(mysqli_error($con));
 	$arrVal = array();
  	$i=1;
+
  	while ($rowList = mysqli_fetch_array($sqltran)) {
- 							 
 		$x = array(
 		    'link'=> $rowList['link'],
+		    'id'=> $rowList['id'],
 			'bron'=> $rowList['bron'],
 			'datum'=> $rowList['datum'],
 		    'titel'=> $rowList['titel'],
-			'inleiding'=> $rowList['inleiding'],
-			'raw'=> $rowList['raw'],
+			'inleiding'=>  base64_decode($rowList['inleiding']),
+			'raw'=> base64_decode($rowList['raw']),
 			'genre'=> $rowList['genre'],
     		);		
 		array_push($arrVal, $x);	
